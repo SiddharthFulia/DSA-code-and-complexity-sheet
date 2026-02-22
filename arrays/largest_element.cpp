b4366d3531ce3f651f1da911ba8d6a4ebff80be9
sid <sid@getpassionfruit.com>
2026-02-22 17:31:44 +0530

    array quesiton 1 done

---
+++ arrays/largest_element.cpp
@@ -0,0 +1,8 @@
+// https://www.geeksforgeeks.org/problems/largest-element-in-array4009/0
+class Solution {
+  public:
+    int largest(vector<int> &arr) {
+        // code here
+        return *max_element(arr.begin(),arr.end());
+    }
+};
