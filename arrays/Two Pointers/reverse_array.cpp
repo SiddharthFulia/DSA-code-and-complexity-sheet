95bbfb13514941acb169ef0f2c737a2e3fcacd16
sid <siddharthfulia7@gmail.com>
2026-02-23 14:21:58 +0530

    array 2nd section 1st question done

---
+++ arrays/Two Pointers/reverse_array.cpp
@@ -0,0 +1,8 @@
+// https://www.geeksforgeeks.org/problems/reverse-an-array/0
+class Solution {
+  public:
+    void reverseArray(vector<int> &arr) {
+        // code here
+        reverse(arr.begin(),arr.end());
+    }
+};
