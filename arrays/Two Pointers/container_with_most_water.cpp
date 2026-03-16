158950ab60cc9aa6bf20ffb87b93b1967e058ffb
unknown <sid@getpassionfruit.com>
2026-03-17 00:53:12 +0530

    5th question 2nd section array done

---
+++ arrays/Two Pointers/container_with_most_water.cpp
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/container-with-most-water/description/
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int i=0,j=height.size()-1,ans=0;
+        while(i<j){
+            ans=max(ans,min(height[i],height[j])*(j-i));
+            if(height[i]<height[j])i++;
+            else j--;
+        }
+        return ans;
+    }
+}; 
